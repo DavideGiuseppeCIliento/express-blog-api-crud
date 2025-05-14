d9a8eab62693b8e54208a905c965c63d8bad0f7f
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-05-14 15:34:38 +0200

    Preparazione del file controller per i posts: postController.js.

---
+++ controllers/postController.js
@@ -0,0 +1,26 @@
+//IMPORT DATA
+let posts = require("../data/array-blog");
+
+// # FUNZIONI CRUD
+
+// INDEX
+
+const index = (req, res) => {};
+
+// SHOW
+const show = (req, res) => {};
+
+// STORE
+const store = (req, res) => {};
+
+// UPDATE
+const update = (req, res) => {};
+
+// MODIFY
+const modify = (req, res) => {};
+
+// DESTROY
+const destroy = (req, res) => {};
+
+//EXPORT CONTROLLER FUNCTION
+module.exports = { index, show, store, update, modify, destroy };
