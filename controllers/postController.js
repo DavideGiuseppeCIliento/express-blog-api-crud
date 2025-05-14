0d176743808e7a6dc4ef78bbc677081d5baa956e
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-05-14 15:43:28 +0200

    Settaggio file controller e router.

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,22 +5,68 @@ let posts = require("../data/array-blog");
 
 // INDEX
 
-const index = (req, res) => {};
+const index = (req, res) => {
+  res.json({
+    message: "Visualizzo tutti i post",
+    posts,
+  });
+};
 
 // SHOW
-const show = (req, res) => {};
+const show = (req, res) => {
+  // Trasformo in INT la stringa ID
+  const id = parseInt(req.params.id);
+  // Trova il post con ID corrispondente
+  const post = posts.find((post) => post.id === id);
+
+  res.json({
+    message: `Visualizzo il post ${id}`,
+    post,
+  });
+};
 
 // STORE
-const store = (req, res) => {};
+const store = (req, res) => {
+  res.json({
+    message: "Aggiungo un post",
+  });
+};
 
 // UPDATE
-const update = (req, res) => {};
+const update = (req, res) => {
+  res.json({
+    message: "Modifico un post",
+  });
+};
 
 // MODIFY
-const modify = (req, res) => {};
+const modify = (req, res) => {
+  res.json({
+    message: "Modifico un post",
+  });
+};
 
 // DESTROY
-const destroy = (req, res) => {};
+const destroy = (req, res) => {
+  // Trasformo in INT la stringa ID
+  const id = parseInt(req.params.id);
+  // Trova il post con ID corrispondente se non c'è error è FALSE
+  const error = posts.find((post) => post.id === id);
+
+  if (!error) {
+    // ERROR FALSE error 404
+    res.status(404).json({
+      message: `Il post ${id} non è stato trovato`,
+    });
+  } else {
+    //Stampa posts rimuovendo il prescelto
+    posts = posts.filter((post) => post.id !== id);
+    res.json({
+      message: `Ho rimosso il post ${id}`,
+      posts,
+    });
+  }
+};
 
 //EXPORT CONTROLLER FUNCTION
 module.exports = { index, show, store, update, modify, destroy };
--- routers/posts.js
+++ routers/posts.js
@@ -4,73 +4,30 @@ const express = require("express");
 //IMPORT DATA
 let posts = require("../data/array-blog");
 
+//IMPORT CONTROLLER FUNCTION
+const postController = { index, show, store, update, modify, destroy };
+
 //SET ROUTER
 const router = express.Router();
 
 // CRUD
 //# INDEX --------------------------------
-router.get("/", (req, res) => {
-  res.json({
-    message: "Visualizzo tutti i post",
-    posts,
-  });
-});
+router.get("/", postController.index);
 
 //# SHOW --------------------------------
-router.get("/:id", (req, res) => {
-  // Trasformo in INT la stringa ID
-  const id = parseInt(req.params.id);
-  // Trova il post con ID corrispondente
-  const post = posts.find((post) => post.id === id);
-
-  res.json({
-    message: `Visualizzo il post ${id}`,
-    post,
-  });
-});
+router.get("/:id", postController.show);
 
 //# STORE --------------------------------
-router.post("/", (req, res) => {
-  res.json({
-    message: "Aggiungo un post",
-  });
-});
+router.post("/", postController.store);
 
 //# UPDATE --------------------------------
-router.put("/:id", (req, res) => {
-  res.json({
-    message: "Modifico un post",
-  });
-});
+router.put("/:id", postController.update);
 
 //# MODIFY --------------------------------
-router.patch("/:id", (req, res) => {
-  res.json({
-    message: "Modifico un post",
-  });
-});
+router.patch("/:id", postController.modify);
 
 //# DESTROY --------------------------------
-router.delete("/:id", (req, res) => {
-  // Trasformo in INT la stringa ID
-  const id = parseInt(req.params.id);
-  // Trova il post con ID corrispondente se non c'è error è FALSE
-  const error = posts.find((post) => post.id === id);
-
-  if (!error) {
-    // ERROR FALSE error 404
-    res.status(404).json({
-      message: `Il post ${id} non è stato trovato`,
-    });
-  } else {
-    //Stampa posts rimuovendo il prescelto
-    posts = posts.filter((post) => post.id !== id);
-    res.json({
-      message: `Ho rimosso il post ${id}`,
-      posts,
-    });
-  }
-});
+router.delete("/:id", postController.destroy);
 
 //EXPORT ROUTER
 module.exports = router;
