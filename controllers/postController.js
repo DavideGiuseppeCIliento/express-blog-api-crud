94ff4a93b599774a79d50f700b5989863c12edf7
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-05-15 16:44:14 +0200

    Aggiunto l'algoritmo del MODIFY.

--- controllers/postController.js
+++ controllers/postController.js
@@ -127,10 +127,66 @@ const update = (req, res) => {
   // Trasformo in INT la stringa ID
   const id = parseInt(req.params.id);
   // Trova il post con ID corrispondente se non c'è error è FALSE
-  const error = removePost.find((post) => removePost.id === id);
+  const postFind = posts.find((post) => post.id === id);
 
+  if (!postFind) {
+    // ERROR FALSE error 404
+    return res.status(404).json({
+      message: `Il post ${id} non è stato trovato`,
+    });
+  }
+
+  // Importiamo i valori dal body
+  const { title, content, image, tags } = req.body;
+
+  // Controlli validità chiavi oggetto
+  if (!title || typeof title !== "string") {
+    res.status(400);
+    res.json({
+      error: "Error 400",
+      message: "Compilazione errata!",
+    });
+    return;
+  }
+
+  if (!content || typeof content !== "string") {
+    res.status(400);
+    res.json({
+      error: "Error 400",
+      message: "Compilazione errata!",
+    });
+    return;
+  }
+
+  if (!image || typeof image !== "string") {
+    res.status(400);
+    res.json({
+      error: "Error 400",
+      message: "Compilazione errata!",
+    });
+    return;
+  }
+
+  if (!tags || !Array.isArray(tags)) {
+    res.status(400);
+    res.json({
+      error: "Error 400",
+      message: "Compilazione errata!",
+    });
+    return;
+  }
+
+  // Modifichiamo l'elemento nell'Array
+  postFind.title = title;
+  postFind.content = content;
+  postFind.image = image;
+  postFind.tags = tags;
+
+  // Messaggio di successo e show dell'array
+  res.status(200);
   res.json({
-    message: "Modifico un post",
+    message: "Visualizzo post modificato",
+    post: postFind,
   });
 };
 
@@ -142,10 +198,66 @@ const modify = (req, res) => {
   // Trasformo in INT la stringa ID
   const id = parseInt(req.params.id);
   // Trova il post con ID corrispondente se non c'è error è FALSE
-  const error = removePost.find((post) => removePost.id === id);
+  const postFind = posts.find((post) => post.id === id);
 
+  if (!postFind) {
+    // ERROR FALSE error 404
+    return res.status(404).json({
+      message: `Il post ${id} non è stato trovato`,
+    });
+  }
+
+  // Importiamo i valori dal body
+  let { title, content, image, tags } = req.body;
+
+  // Controlliamo che i valori importati siano uguali o diversi dagli originali
+  title = title ?? postFind.title;
+  content = content ?? postFind.content;
+  image = image ?? postFind.image;
+  tags = tags ?? postFind.tags;
+
+  // Controlli validità chiavi oggetto
+  if (typeof title !== "string") {
+    res.status(400);
+    res.json({
+      error: "Error 400",
+      message: "Compilazione errata!",
+    });
+    return;
+  }
+
+  if (typeof content !== "string") {
+    res.status(400);
+    res.json({
+      error: "Error 400",
+      message: "Compilazione errata!",
+    });
+    return;
+  }
+
+  if (typeof image !== "string") {
+    res.status(400);
+    res.json({
+      error: "Error 400",
+      message: "Compilazione errata!",
+    });
+    return;
+  }
+
+  if (!Array.isArray(tags)) {
+    res.status(400);
+    res.json({
+      error: "Error 400",
+      message: "Compilazione errata!",
+    });
+    return;
+  }
+
+  // Messaggio di successo e show dell'array
+  res.status(200);
   res.json({
-    message: "Modifico un post",
+    message: "Visualizzo post modificato",
+    post: postFind,
   });
 };
 
