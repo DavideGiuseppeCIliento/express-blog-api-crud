f0d20b72d6f6231293f3f1f9ee02f9294d5446b5
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-05-17 15:38:47 +0200

    Ho aggiunto la gestione degli errori e il Not Found.

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ const port = 3000;
 
 //IMPORT ROUTER POSTS
 const routerPosts = require("./routers/posts");
+const errorHeandler = require("./middlewares/errorheadler");
+
+//IMPORT MIDDLEWARE ERROR
+const errorheadler = require("./middleware/errorHadler");
+
+//IMPORT MIDDLEWARE NOT FOUND
+const notFound = require("./middlewares/notFound");
+
+//----------------------------------------------------------
 
 //# SET PUBLIC DIRECTORY --------------------------------
 app.use(express.static(`public`));
@@ -20,6 +29,14 @@ app.use(express.json());
 
 //# SET RUTER POSTS --------------------------------
 app.use("/posts", routerPosts);
+
+//# MIDDLEWARE ERROR
+app.use(errorHeandler);
+
+//# MIDDLEWARE NOT FOUND
+app.use(notFound);
+
+//----------------------------------------------------------
 
 //# LISTEN PORT ---------------------------------
 app.listen(port, (error) => {
---
+++ middlewares/notFound.js
@@ -0,0 +1,9 @@
+const notFound = (err, req, res, next) => {
+  res.status(404);
+  res.json({
+    error: "Not Found",
+    message: "Pagina non trovata",
+  });
+};
+
+module.exports = notFound;
